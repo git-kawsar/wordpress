22d7e184c6c603a957ce33154391ebd39b1670c3
git-kawsar <45220936+git-kawsar@users.noreply.github.com>
2019-07-18 12:39:27 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,12 +4,12 @@ function my_function(){
   add_theme_support('title-tag');
   add_theme_support('post-thumbnail');
   add_theme_support('custom-header', array(
-    'default-text-color' => '#c0000',
-    'header-text' => true
-  ));
+    'default-text-color' => '#c0000', // get_header_textcolor();
+    'header-text' => true // if(!display_header_text()){ echo "display: none;"; }
+  )); // header_image();
   add_theme_support('custom-logo', array(
     'width' => '100',
     'height' => '100'
-  ));
+  )); // the_custom_logo();
 }
 add_action('after_setup_theme', 'my_functin');
