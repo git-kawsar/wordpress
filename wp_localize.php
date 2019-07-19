e641d684d1e149e57e7c3448ab0bf9e3eca99ff5
git-kawsar <45220936+git-kawsar@users.noreply.github.com>
2019-07-19 19:44:16 +0600

    Update wp_localize.php

--- wp_localize.php
+++ wp_localize.php
@@ -1,8 +1,11 @@
 <?php
-	// send value to js by php in wordpress
+// send value to js by php in wordpress
 	//first generate the value (whatever you want) to send 
 	$prefix_date = get_post_meta($post->ID, 'meta_key', true);
 	wp_localize_script($handler, $data, array(
 		'date' => $prefix_date
 	));
+	// than set your $data to your js file
+	'date' => $data.date
+	
 	
