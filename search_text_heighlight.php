6aebf4d480bdf3ca1d42198f6b87192ad9926fa2
git-kawsar <45220936+git-kawsar@users.noreply.github.com>
2019-07-24 21:49:16 +0600

    Create search_text_heighlight.php

---
+++ search_text_heighlight.php
@@ -0,0 +1,10 @@
+function alpha_highlight_search_results($text){
+    if(is_search()){
+        $pattern = '/('. join('|', explode(' ', get_search_query())).')/i';
+        $text = preg_replace($pattern, '<span class="search-highlight">\0</span>', $text);
+    }
+    return $text;
+}
+add_filter('the_content', 'alpha_highlight_search_results');
+add_filter('the_excerpt', 'alpha_highlight_search_results');
+add_filter('the_title', 'alpha_highlight_search_results');
